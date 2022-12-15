b772fdb0262f11d3b2790b152bbc4c7b375a844b
yamashi <yamashi@users.noreply.github.com>
2022-12-15 11:08:48 +0100

    vtable for IRTTIType

--- include/red3lib/CRTTISystem.hpp
+++ include/red3lib/CRTTISystem.hpp
@@ -11,7 +11,34 @@ namespace red3lib
 {
 struct IClassBuilder;
 struct CFunction;
-struct IRTTIType;
+struct IFile;
+
+struct IRTTIBaseObject
+{
+    virtual ~IRTTIBaseObject() = 0;
+};
+
+struct IRTTIType
+{
+    virtual ~IRTTIType() = 0;                                           // 00
+    virtual CName GetName() const = 0;                                  // 08
+    virtual uint32_t GetSize() const = 0;                               // 10
+    virtual uint32_t GetAlignment() const = 0;                          // 18
+    virtual uint32_t GetType() const = 0;                               // 20
+    virtual uint32_t GetUnkFlag1() const = 0;                           // 28
+    virtual uint32_t GetUnkFlag2() const = 0;                           // 30
+    virtual void Construct(void* aPtr) const = 0;                       // 38
+    virtual void Destruct(void* aPtr) const = 0;                        // 40
+    virtual bool Compare(void* aPtr1, void* aPtr2) const = 0;           // 48
+    virtual bool Copy(void* aOut, const void* aPtr) const = 0;          // 50
+    virtual bool Clean(void* aOut) const = 0;                           // 58
+    virtual bool Serialize(IFile* aOut, void* aPtr) const = 0;          // 60
+    virtual bool ToString(void* aPtr, void* aString) const = 0;         // 68
+    virtual bool FromString(void* aPtr, void* aString) const = 0;       // 70
+    virtual bool DebugValidate(void* aPtr, void* aString) const = 0;    // 78
+    virtual uint32_t GetUnkFlag3() const = 0;                           // 80
+    virtual uint32_t GetUnkFlag4() const = 0;                           // 88
+};
 
 struct CRTTISystem
 {
